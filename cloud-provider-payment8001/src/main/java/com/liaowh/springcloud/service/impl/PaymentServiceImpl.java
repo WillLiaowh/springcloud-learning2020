b9d1237ba99c89b6aa742210e57e7fbc32266eaa
liaowanghui <liaowanghui@chinamobile.com>
2020-05-13 18:01:34 +0800

    8001

---
+++ cloud-provider-payment8001/src/main/java/com/liaowh/springcloud/service/impl/PaymentServiceImpl.java
@@ -0,0 +1,31 @@
+package com.liaowh.springcloud.service.impl;
+
+import com.liaowh.springcloud.dao.PaymentDao;
+import com.liaowh.springcloud.entities.Payment;
+import com.liaowh.springcloud.service.PaymentService;
+import org.springframework.stereotype.Service;
+
+import javax.annotation.Resource;
+
+/**
+ * @auther zzyy
+ * @create 2020-02-18 10:40
+ */
+@Service
+public class PaymentServiceImpl implements PaymentService
+{
+    @Resource
+    private PaymentDao paymentDao;
+
+    @Override
+    public int create(Payment payment)
+    {
+        return paymentDao.create(payment);
+    }
+
+    @Override
+    public Payment getPaymentById(Long id)
+    {
+        return paymentDao.getPaymentById(id);
+    }
+}
