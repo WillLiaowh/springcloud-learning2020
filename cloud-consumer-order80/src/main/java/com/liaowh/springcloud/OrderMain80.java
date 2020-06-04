a8ea5fe0591c79ab9a99c8871e6da39b4118798c
liaowanghui <liaowanghui@chinamobile.com>
2020-06-04 09:17:21 +0800

    zk consul openfeign

--- cloud-consumer-order80/src/main/java/com/liaowh/springcloud/OrderMain80.java
+++ cloud-consumer-order80/src/main/java/com/liaowh/springcloud/OrderMain80.java
@@ -1,11 +1,14 @@
 package com.liaowh.springcloud;
 
+import com.liaowh.myruler.MySelfRule;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.ribbon.RibbonClient;
 
 @SpringBootApplication
 @EnableEurekaClient
+@RibbonClient(name = "CLOUD-PAYMENT-SERVICE",configuration= MySelfRule.class)
 public class OrderMain80
 {
     public static void main(String[] args) {
